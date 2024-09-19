9857920dfc6502df977cfde7d262f446d7b7f60a
Malith Kuruppu <info.mkappworks@gmail.com>
2024-09-19 22:11:14 +0200

    Removed unused import

--- src/main/java/com/mkappworks/springsecuritydemo/repo/UserRepo.java
+++ src/main/java/com/mkappworks/springsecuritydemo/repo/UserRepo.java
@@ -2,7 +2,6 @@ package com.mkappworks.springsecuritydemo.repo;
 
 import com.mkappworks.springsecuritydemo.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepo extends JpaRepository<Users, Integer> {
     Users findByUsername(String username);
